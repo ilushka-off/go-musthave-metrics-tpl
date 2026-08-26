faf5a794d7f91b54dbc614b9115463dd8566a3de
Ilya <mnailJk@yandex.ru>
2026-08-26 18:49:58 +0300

    feat: add logger.go file new func

---
+++ internal/middleware/logger.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	size       int
+}
+
+func (w *loggingResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *loggingResponseWriter) Write(b []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+	size, err := w.ResponseWriter.Write(b)
+
+	w.size += size
+
+	return size, err
+}
+
+func Logger(log *zap.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			lw := &loggingResponseWriter{ResponseWriter: w}
+			next.ServeHTTP(lw, r)
+
+			duration := time.Since(start)
+
+			log.Info("Log Info",
+				zap.String("uri", r.RequestURI),
+				zap.String("method", r.Method),
+				zap.Duration("duration", duration),
+				zap.Int("status", lw.statusCode),
+				zap.Int("size", lw.size),
+			)
+		})
+	}
+}
