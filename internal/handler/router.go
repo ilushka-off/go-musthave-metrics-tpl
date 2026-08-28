301ee4dbd23d14a499ad6bdd45ce6d91abe1b797
Ilya <mnailJk@yandex.ru>
2026-08-28 16:35:26 +0300

    fix: update router.go add two new middleware call

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -9,6 +9,8 @@ import (
 func NewRouter(h *MetricsHandler, log *zap.Logger) chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger(log))
+	r.Use(middleware.GzipDecompress())
+	r.Use(middleware.GzipCompress())
 	r.Post("/update/{type}/{name}/{value}", h.Update)
 	r.Post("/update", h.UpdateJSON)
 	r.Get("/value/{type}/{name}", h.Value)
